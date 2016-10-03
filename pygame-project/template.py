f279890cfdd7e68286965fd5f364b46e2b809abf
Robert Dunn <robertdunn220@gmail.com>
2016-10-03 16:36:17 -0400

    Started the game with pygame

--- pygame-project/template.py
+++ pygame-project/template.py
@@ -1,34 +1,47 @@
 import pygame
 import random
 
-class Ball(object):
+class Hero(object):
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.speed_x = 5
         self.speed_y = 5
-        self.radius = 50
+
+    def render(self, screen):
+        hero = pygame.image.load('hero.png')
+        screen.blit(hero, (self.x, self.y))
+
+class Monster(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.speed_x = 5
+        self.speed_y = 5
 
     def update(self, width, height):
         self.x += self.speed_x
-        self.y += self.speed_y
-        if self.x + self.radius > width:
-            self.speed_x = -5
-        if self.y + self.radius > height:
-            self.speed_y = -5
-        if self.x - self.radius < 0:
-            self.speed_x = 5
-        if self.y - self.radius < 0:
-            self.speed_y = 5
+        #self.y += (self.speed_y)/4
+        if self.x > width:
+            self.x = 0
+            rand_num = random.randrange(20, 480)
+            self.y = rand_num
+        if self.x < 20:
+            self.speed_x = 6
+        if self.y > height - 50:
+            self.speed_y = -2
+        if self.y < 20:
+            self.speed_y = 6
 
     def render(self, screen):
-        pygame.draw.circle(screen, (255, 0, 0), (self.x, self.y), self.radius)
+        monster = pygame.image.load('monster.png')
+        screen.blit(monster, (self.x, self.y))
 
 def main():
     # declare the size of the canvas
-    width = 500
-    height = 500
-    blue_color = (97, 159, 182)
+    width = 512
+    height = 480
+    bg = pygame.image.load('background.png')
 
     # initialize the pygame framework
     pygame.init()
@@ -37,7 +50,7 @@ def main():
     screen = pygame.display.set_mode((width, height))
 
     # set window caption
-    pygame.display.set_caption("Rob's Game")
+    pygame.display.set_caption('Simple Example')
 
     # create a clock
     clock = pygame.time.Clock()
@@ -45,30 +58,21 @@ def main():
     ################################
     # PUT INITIALIZATION CODE HERE #
     ################################
-    ball_list = [
-        Ball(50, 50),
-        Ball(200, 50),
-        Ball(50, 200),
-        Ball(50, 300),
-        Ball(300, 50)
-        ]
+    monster = Monster(0, 180)
+    hero = Hero(236, 210)
 
     # game loop
     stop_game = False
     while not stop_game:
         # look through user events fired
         for event in pygame.event.get():
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                print "Click at coordinate %d, %d" % event.pos
-
-
-
-                y = random.randrange(0,500)
-                x = random.randrange(0,500)
-
-                ball_list.append(Ball(event.pos))
-
+            ################################
+            # PUT EVENT HANDLING CODE HERE #
+            ################################
+            if event.type == pygame.KEYUP:
+                hero.y -= 10
+            #elif event.type == pygame.KEYDOWN:
+                #hero.y += 10
             if event.type == pygame.QUIT:
                 # if they closed the window, set stop_game to True
                 # to exit the main loop
@@ -77,17 +81,17 @@ def main():
         #######################################
         # PUT LOGIC TO UPDATE GAME STATE HERE #
         #######################################
-        for ball in ball_list:
-            ball.update(width, height)
+
 
         # fill background color
-        screen.fill(blue_color)
+        screen.blit(bg, (0, 0))
 
+        hero.render(screen)
+        monster.update(width, height)
+        monster.render(screen)
         ################################
         # PUT CUSTOM DISPLAY CODE HERE #
         ################################
-        for ball in ball_list:
-            ball.render(screen)
 
         # update the canvas display with the currently drawn frame
         pygame.display.update()
